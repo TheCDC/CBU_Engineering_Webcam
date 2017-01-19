6343c020e001d243cd28d5c288fd45346c87a70b
christopher <christoipher.chen@gmail.com>
2017-01-19 07:13:35 -0800

    Reverted back to previous time check method

--- robot.py
+++ robot.py
@@ -11,7 +11,6 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 def main():
     logging.info("Started")
     # intialize timer to ensure correct time in between shots.
-    last_shot = time.time() - 60
 
     try:
         # wait until the beginning of the next minute
@@ -19,28 +18,26 @@ def main():
             time.sleep(0.01)
 
         while True:
-            now = time.time()
             # only shoot on the bottom of each minute
-            if now >= last_shot + 60:
-                last_shot = now
-                # don't shoot multiple times per second
-                print(time_utils.now())
-                # shoot first
-                try:
-                    logging.info("Shooting")
-                    shoot.shoot()
-                # ask questions later
+            while time.localtime().tm_sec != 0:
+                time.sleep(0.01)
+            # don't shoot multiple times per second
+            print(time_utils.now())
+            # shoot first
+            try:
+                logging.info("Shooting")
+                shoot.shoot()
+            # ask questions later
 
-                except (KeyboardInterrupt, EOFError) as e:
-                    raise e
-                except requests.exceptions.ConnectionError as e:
-                    logging.warning(e)
-                    print(e)
-                except Exception as e:
-                    logging.warning(e)
-                    raise e
+            except (KeyboardInterrupt, EOFError) as e:
+                raise e
+            except requests.exceptions.ConnectionError as e:
+                logging.warning(e)
+                print(e)
+            except Exception as e:
+                logging.warning(e)
+                raise e
             # delay to avoid wasting CPU cycles
-            time.sleep(0.01)
     except (EOFError, KeyboardInterrupt) as e:
         # handle user-requested termination
         logging.info("Stopped")
